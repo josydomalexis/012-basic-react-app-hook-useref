c751ca25f47c4cb93b1e8b00e30571daabf295d7
Josy Dom Alexis <josyanimator@gmail.com>
2024-01-29 19:34:27 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
+
+  const ageRef = useRef();
+  const [age, setAge] = useState(0)
+  
+  const handleAge = (e) =>{
+    e.preventDefault()
+    setAge(ageRef.current.value)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+          <tr>
+            <td>Age</td>
+          </tr>
+          <tr>
+            <td>
+              <form >
+              <input type="number" ref={ageRef} placeholder="..." style={{margin:"10px 0",padding:"5px 10px",textAlign:"center", fontSize:"1.5rem"}}/>
+              </form>
+            </td>
+          </tr>
+          <tr>
+          <button onClick={(e)=>handleAge(e)}>Submit</button>
+          </tr>
+        </table>
+        <div>
+          <br/><br/><br/>
+          {age}
+        </div>
       </header>
     </div>
   );
